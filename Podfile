3139a8af5d6ac048a207f766edbc703b74f28179
Paul Leclerc <paulleclerc@icloud.com>
2019-11-14 23:58:20 +0100

    added keyboard management
    Lol j'y ai passé toute la soirée pour finalement me rendre compte qu'un pod s'en charger parfaitement

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 target 'VigiClean' do
   pod 'Firebase/Analytics'
   pod 'Firebase/Auth'
+  pod 'IQKeyboardManagerSwift'
   target 'VigiCleanTests' do
     inherit! :search_paths
    end
--- VigiClean/AppDelegate.swift
+++ VigiClean/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        IQKeyboardManager.shared.enable = true
+        
         try? Auth.auth().signOut()
         return true
     }
