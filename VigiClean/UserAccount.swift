69ad7a72e9c5ffb9768052a02a65f3af1bb8b004
Paul Leclerc <paulleclerc@icloud.com>
2019-11-18 01:22:59 +0100

    reindented

--- VigiClean/UserAccount.swift
+++ VigiClean/UserAccount.swift
@@ -15,21 +15,38 @@ class UserAccount {
     }
     
     static func signUp(username: String, email: String, password: String, completion: @escaping((Error?) -> Void)) {
-        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in // swiftlint:disable:this unused_closure_parameter line_length
-            
-            guard error == nil else { // if no error, user is created
-                completion(error)
-                return
-            }
-
-            completion(nil)
-            return
+        Auth.auth().createUser(withEmail: email,
+                               password: password) { (authResult, error) in
+                                
+                                guard error == nil,
+                                    let user = authResult?.user else { // if no error, user is created
+                                        completion(error)
+                                        return
+                                }
+                                
+                                createUserDocument(for: user, named: username, merge: true) { error in
+                                    completion(error)
+                                }
+                                
+                                completion(nil)
+                                return
         }
     }
     
     static func signIn(email: String, password: String, completion: @escaping((Error?) -> Void)) {
-        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in // swiftlint:disable:this unused_closure_parameter line_length
-            completion(error)
+        Auth.auth().signIn(withEmail: email,
+                           password: password) { (authResult, error) in
+                            guard error == nil,
+                                let user = authResult?.user else {
+                                    completion(error)
+                                    return
+                            }
+                            
+                            createUserDocument(for: user, named: email, merge: true) { error in
+                                print(error ?? "An unknown error occured while creating user document in method signIn(email:password:completion:") // swiftlint:disable:this line_length
+                            }
+                            
+                            completion(nil)
         }
     }
     
@@ -51,11 +68,18 @@ class UserAccount {
         return errCode
     }
     
-    private static func createUserDocument(for user: User, named: String?, completion: @escaping (Error?) -> Void) {
-        let db = Firestore.firestore()
+    private static func createUserDocument(for user: User,
+                                           named: String?,
+                                           merge: Bool,
+                                           completion: @escaping (Error?) -> Void) {
+        let database = Firestore.firestore()
         let uid = user.uid // getting uid to create user's document
-        db.collection("User").document(uid).setData(["credits": 0, "lastName": NSNull(), "firstName": NSNull(), "username": named ?? uid]) { error in // creating user's document ; Deleting user if error
-            completion(error)
+        database.collection("User").document(uid).setData(["credits": 0,
+                                                           "lastName": NSNull(),
+                                                           "firstName": NSNull(),
+                                                           "username": named ?? uid],
+                                                          merge: merge) { error in // creating user's document
+                                                            completion(error)
         }
         
         completion(nil)
