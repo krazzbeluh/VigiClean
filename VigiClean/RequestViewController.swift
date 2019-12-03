b63525f28c44e24c9a7d3cce6dec07edc02de71d
Paul Leclerc <paulleclerc@icloud.com>
2019-12-03 17:36:19 +0100

    tested way to display menu in textField instead of pickerView

--- VigiClean/RequestViewController.swift
+++ VigiClean/RequestViewController.swift
@@ -9,23 +9,29 @@
 import UIKit
 
 class RequestViewController: UIViewController, RequestView {
+    let testArrayForPickerView = ["Coucou", "La poubelle est pleine", "La poubelle est endommagée"]
     
     var presenter: RequestViewPresenter!
     
     // MARK: Properties
-
+    let myPicker = UIPickerView()
+    
     // MARK: Outlets
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var grayOutView: UIView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var organizationLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var action: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         presenter = RequestPresenter(view: self)
         
+        myPicker.delegate = self
+        myPicker.dataSource = self
+        action.inputView = myPicker
     }
     
     // MARK: Methods
@@ -40,4 +46,22 @@ class RequestViewController: UIViewController, RequestView {
         typeLabel.text = object.type
     }
     
+}
+
+extension RequestViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return testArrayForPickerView.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return testArrayForPickerView[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        action.text = testArrayForPickerView[row]
+    }
 }
